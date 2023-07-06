7f4f90a9f895b5c5aeecc7e8179bded4bda5e742
Xoritzium <tom.thiede@gmx.de>
2023-07-06 09:20:34 +0200

    better UI for Scanning State feedback
    TODO:
    -> remove the AR-Plane material

--- Assets/Assets-Atrium/Skripts/ControlUserInputsManager.cs
+++ Assets/Assets-Atrium/Skripts/ControlUserInputsManager.cs
@@ -6,70 +6,83 @@ using UnityEngine.UI;
 
 public class ControlUserInputsManager : MonoBehaviour
 {
-    [SerializeField]
-    TextMeshProUGUI infoText;
-    [SerializeField, TextArea]
-    string infoTextText;
+	[SerializeField]
+	TextMeshProUGUI infoText;
+	[SerializeField, TextArea]
+	string infoTextText;
 
 
-    [SerializeField]
-    Slider slider;
-    [SerializeField]
-    GameObject spawnBTN;
-    [SerializeField]
-    Image mask;
-    [SerializeField]
-    TextMeshProUGUI scanningState;
-    [SerializeField]
-    string scanningMessage;
-    [SerializeField]
-    string scanningMessageDone;
-    [SerializeField, Tooltip("Count of planes which are generated to proceed to the mask")]
-    int detectedPlaneFragments;
+	[SerializeField]
+	Slider slider;
+	[SerializeField]
+	GameObject spawnBTN;
+	[SerializeField]
+	Image mask;
+	[SerializeField]
+	GameObject scanningBar, scanningParent;
 
-    GameObject origin;
-    GameObject trackables;
-    ControlAnimation controlAnimation;
-    ARContentManager contentManager;
+	[SerializeField, Tooltip("Count of planes which are generated to proceed to the mask")]
+	int detectedPlaneFragments;
 
-    bool tracking = true;
+	GameObject origin;
+	GameObject trackables;
+	ControlAnimation controlAnimation;
+	ARContentManager contentManager;
 
-    private void Start() {
-        infoText.text = infoTextText;
+	bool tracking = true;
 
-        origin = GameObject.Find("XR Origin");
-        controlAnimation = GetComponent<ControlAnimation>();
-        contentManager = origin.GetComponent<ARContentManager>();
-        slider.gameObject.SetActive(false);
-        spawnBTN.SetActive(false);
-        mask.gameObject.SetActive(false);
-        scanningState.text = scanningMessage;
-        trackables = GameObject.Find("Trackables");
-        Debug.Log("trackables: " + trackables);
-        Debug.Log("detected Planes: " + detectedPlaneFragments);
 
-    }
+	//testing
 
-    private void Update() {
-        if (trackables.transform.childCount > detectedPlaneFragments && tracking) {
-            tracking = false;
-            trackingDone();
-        }
-    }
+	private void Start()
+	{
+		infoText.text = infoTextText;
+		scanningBar.transform.localScale = new(0, 1, 1);
+		origin = GameObject.Find("XR Origin");
+		controlAnimation = GetComponent<ControlAnimation>();
+		contentManager = origin.GetComponent<ARContentManager>();
+		slider.gameObject.SetActive(false);
+		spawnBTN.SetActive(false);
+		mask.gameObject.SetActive(false);
+		trackables = GameObject.Find("Trackables");
+		Debug.Log("trackables: " + trackables);
+		Debug.Log("detected Planes: " + detectedPlaneFragments);
 
-    private void trackingDone() {
-        scanningState.text = scanningMessageDone;
-        mask.gameObject.SetActive(true);
-        spawnBTN.gameObject.SetActive(true);
-    }
+	}
 
-    public void contentSpawned() {
-        mask.gameObject.SetActive(false);
-        scanningState.transform.parent.gameObject.SetActive(false);
-        spawnBTN.gameObject.SetActive(false);
-        slider.gameObject.SetActive(true);
-        controlAnimation.ActivateContent();
-    }
+	private void Update()
+	{
+		if (trackables.transform.childCount > detectedPlaneFragments && tracking)
+		{
+			tracking = false;
+			trackingDone();
+		}
+		else
+		{
+			setScaleOfScanningBar(trackables.transform.childCount);
+		}
+	}
 
+	private void trackingDone()
+	{
+		scanningParent.SetActive(false);
+		mask.gameObject.SetActive(true);
+		spawnBTN.gameObject.SetActive(true);
+	}
+
+	public void contentSpawned()
+	{
+		mask.gameObject.SetActive(false);
+		scanningBar.transform.parent.gameObject.SetActive(false);
+		spawnBTN.gameObject.SetActive(false);
+		slider.gameObject.SetActive(true);
+		controlAnimation.ActivateContent();
+	}
+
+
+	private void setScaleOfScanningBar(int scale)
+	{
+		scanningBar.transform.localScale = new((float)scale / 5, 1, 1);
+	}
 
 }
